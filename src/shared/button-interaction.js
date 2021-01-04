b0934f8c0834f3e4d160d585fd65b615f5309f2b
Emile Fokkema <emilefokkema@gmail.com>
2021-01-04 21:50:58 +0100

    check for last error

--- src/shared/button-interaction.js
+++ src/shared/button-interaction.js
@@ -1,9 +1,19 @@
 var buttonInteraction = {
     setBadgeText({tabId, text}){
-        chrome.browserAction.setBadgeText({tabId, text});
+        chrome.browserAction.setBadgeText({tabId, text}, () => {
+            var lastError = chrome.runtime.lastError;
+            if(lastError){
+                console.log(`there was an error setting browser action badge text for tab ${tabId}: ${e.message}`);
+            }
+        });
     },
     setBadgeBackgroundColor({tabId, color}){
-        chrome.browserAction.setBadgeBackgroundColor({tabId, color});
+        chrome.browserAction.setBadgeBackgroundColor({tabId, color}, () => {
+            var lastError = chrome.runtime.lastError;
+            if(lastError){
+                console.log(`there was an error setting browser action background color for tab ${tabId}: ${e.message}`);
+            }
+        });
     }
 };
 
