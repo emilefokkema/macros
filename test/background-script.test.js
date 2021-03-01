bdebe8a22b48273c68f453d371622b691b9528ac
Emile Fokkema <emilefokkema@gmail.com>
2021-03-01 10:36:09 +0100

    add test

---
+++ test/background-script.test.js
@@ -0,0 +1,8 @@
+const {backgroundScript} = require('../src/background/background-script-function');
+
+describe('the background script function', () => {
+
+    it('should', () => {
+        expect(true).toBe(true);
+    });
+});
