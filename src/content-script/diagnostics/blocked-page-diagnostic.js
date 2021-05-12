7ab5f83875d2f9f4c1a13af57e7e27a2925b3101
Emile Fokkema <emilefokkema@gmail.com>
2021-05-12 22:31:22 +0200

    step

--- src/content-script/diagnostics/blocked-page-diagnostic.js
+++ src/content-script/diagnostics/blocked-page-diagnostic.js
@@ -1,5 +1,55 @@
+class NodeFilterContext{
+    constructor(node){
+        this.style = getComputedStyle(node);
+    }
+}
+
+class NotFilter{
+    constructor(filter){
+        this.filter = filter;
+    }
+    nodePassesFilter(nodeContext){
+        return !this.filter.nodePassesFilter(nodeContext);
+    }
+}
+
+class AndFilter{
+    constructor(filter1, filter2){
+        this.filter1 = filter1;
+        this.filter2 = filter2;
+    }
+    nodePassesFilter(nodeContext){
+        return this.filter1.nodePassesFilter(nodeContext) && this.filter2.nodePassesFilter(nodeContext);
+    }
+}
+
+class StylePropertyFilter{
+    constructor(property, valueMatcher){
+        this.property = property;
+        this.valueMatcher = valueMatcher;
+    }
+    nodePassesFilter(nodeContext){
+        const value = nodeContext.style.getPropertyValue(this.property);
+        return this.valueMatcher.valuePassesFilter(value);
+    }
+}
+
+class StringValueFilter{
+    constructor(filterValue){
+        this.filterValue = filterValue;
+    }
+    valuePassesFilter(value){
+        return value === this.filterValue;
+    }
+}
+
 export class BlockedPageDiagnostic{
     diagnose(){
-        console.log(`diagnosing!`)
+        const filter = new AndFilter(
+            new StylePropertyFilter('position', new StringValueFilter('fixed')),
+            new NotFilter(new StylePropertyFilter('visibility', new StringValueFilter('hidden')))
+        );
+        const pass = [].filter.apply(document.querySelectorAll('*'), [n => filter.nodePassesFilter(new NodeFilterContext(n))]);
+        console.log(`pass:`, pass)
     }
 }
