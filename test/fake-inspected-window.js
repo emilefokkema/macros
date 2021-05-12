2b54cdd4544165b9e680de6c299c982a5050398d
Emile Fokkema <emilefokkema@gmail.com>
2021-05-12 11:50:41 +0200

    fix tests

--- test/fake-inspected-window.js
+++ test/fake-inspected-window.js
@@ -2,6 +2,6 @@ export class FakeInspectedWindow{
     constructor(tabId){
         this._tabId = tabId || 0;
     }
-    get tabId(){return this._tabId;}
+    async getTabId(){return this._tabId;}
     eval(){}
 }
