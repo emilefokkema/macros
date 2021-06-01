53fc0f9a05ade1f5a5c9926fb17213004d37cb47
Emile Fokkema <emilefokkema@gmail.com>
2021-06-01 20:59:19 +0200

    improve suggestion appearance

--- src/content-script/suggestions/element-indicator.js
+++ src/content-script/suggestions/element-indicator.js
@@ -10,13 +10,13 @@ export class ElementIndicator{
         const zIndexValueString = elementStyle.getPropertyValue('z-index');
         const zIndexValue = isNaN(zIndexValueString) ? 0 : parseInt(zIndexValueString);
         this.indicator.style.zIndex = `${zIndexValue + 1}`;
-        this.indicator.style.width = `${rect.width}px`;
-        this.indicator.style.height = `${rect.height}px`;
+        this.indicator.style.width = `${Math.max(rect.width, 1)}px`;
+        this.indicator.style.height = `${Math.max(rect.height, 1)}px`;
         this.indicator.style.left = `${rect.left}px`;
         this.indicator.style.top = `${rect.top}px`;
         document.body.appendChild(this.indicator);
     }
-    stopIndicatingElement(el){
+    stopIndicatingElement(){
         if(this.indicator.parentElement !== null){
             this.indicator.parentElement.removeChild(this.indicator);
         }
